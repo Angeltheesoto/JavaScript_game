096088f7c378d41fa5cf192728861e5e23d2bd26
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-06-06 22:37:56 -0400

    Added background sound, and was able to toggle it on and off.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 
+// Toggle background audio
+var audio = document.getElementById('background-audio');
 
+document.getElementById('mute').addEventListener('click', function (e)
+{
+ e = e || window.event;
+ audio.muted = !audio.muted;
+ e.preventDefault();
+});
 
 
 
